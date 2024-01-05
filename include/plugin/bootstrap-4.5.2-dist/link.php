4dbdf9d0d6d220165f0920c5bb725d46aac3fcea
itisiriri <117071101+itisiriri@users.noreply.github.com>
2024-01-05 13:40:07 +0800

    Add files via upload

---
+++ include/plugin/bootstrap-4.5.2-dist/link.php
@@ -0,0 +1,13 @@
+
+<link rel='stylesheet' type='text/css' href='<?php echo HREF_PLUGIN_PATH ?>bootstrap-4.5.2-dist/css/bootstrap.min.css' media='screen'/>
+<script type="text/javascript" src="<?php echo HREF_PLUGIN_PATH ?>jquery-3.3.1/jquery-3.3.1.min.js"></script>
+
+<link rel='stylesheet' type='text/css' href='<?php echo HREF_PLUGIN_PATH ?>DataTables/datatables.min.css' media='screen'/>
+
+<script type='text/javascript' src='<?php echo HREF_PLUGIN_PATH ?>DataTables/datatables.min.js'></script>
+
+<script type='text/javascript' src='<?php echo HREF_PLUGIN_PATH ?>popper.js-1.14.4/dist/umd/popper.min.js'></script>
+
+<script type='text/javascript' src='<?php echo HREF_PLUGIN_PATH ?>bootstrap-4.5.2-dist/js/bootstrap.min.js'></script>
+
+<script src="<?php echo HREF_PLUGIN_PATH ?>mCustomScrollbar/malihu-custom-scrollbar-plugin-master/jquery.mCustomScrollbar.concat.min.js"></script>
