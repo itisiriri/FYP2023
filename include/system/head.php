fe12ccf935a8cb8ab2a63356c942f74d4fb104a2
itisiriri <117071101+itisiriri@users.noreply.github.com>
2024-02-17 00:01:42 +0800

    Update head.php

--- include/system/head.php
+++ include/system/head.php
@@ -30,14 +30,14 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
 
-<script type="text/javascript" src="/var/www/FYP2023.com/include/plugin/jquery-3.3.1/jquery-3.3.1.min.js"></script>
-<link rel='stylesheet' type='text/css' href='/var/www/FYP2023.com/include/plugin/DataTables/datatables.min.css' media='screen'/>
-<script type='text/javascript' src='/var/www/FYP2023.com/include/plugin/DataTables/datatables.min.js'></script>
-<script type='text/javascript' src='/var/www/FYP2023.com/include/plugin/popper.js-1.14.4/dist/umd/popper.min.js'></script>
-<script type='text/javascript' src='/var/www/FYP2023.com/include/plugin/bootstrap-4.5.2-dist/js/bootstrap.min.js'></script>
-<script type='text/javascript' src='/var/www/FYP2023.com/include/plugin/mainjs/mainjs.js'></script>
-<link rel='stylesheet' type='text/css' href='/var/www/FYP2023.com/include/plugin/DataTables/DataTables-1.10.22/Responsive-2.2.7/css/responsive.dataTables.min.css' media='screen'/>
-<script type='text/javascript' src='/var/www/FYP2023.com/include/plugin/DataTables/DataTables-1.10.22/Responsive-2.2.7/js/dataTables.responsive.min.js'></script>
+<script type="text/javascript" src="/fyp/include/plugin/jquery-3.3.1/jquery-3.3.1.min.js"></script>
+<link rel='stylesheet' type='text/css' href='/fyp/include/plugin/DataTables/datatables.min.css' media='screen'/>
+<script type='text/javascript' src='/fyp/include/plugin/DataTables/datatables.min.js'></script>
+<script type='text/javascript' src='/fyp/include/plugin/popper.js-1.14.4/dist/umd/popper.min.js'></script>
+<script type='text/javascript' src='/fyp/include/plugin/bootstrap-4.5.2-dist/js/bootstrap.min.js'></script>
+<script type='text/javascript' src='/fyp/include/plugin/mainjs/mainjs.js'></script>
+<link rel='stylesheet' type='text/css' href='/fyp/include/plugin/DataTables/DataTables-1.10.22/Responsive-2.2.7/css/responsive.dataTables.min.css' media='screen'/>
+<script type='text/javascript' src='/fyp/include/plugin/DataTables/DataTables-1.10.22/Responsive-2.2.7/js/dataTables.responsive.min.js'></script>
 
 <!-- plugin -->
 				
