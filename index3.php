2d3ee1a3a759d92298b4a520009b77fa4f987770
itisiriri <117071101+itisiriri@users.noreply.github.com>
2024-02-16 22:30:02 +0800

    Create index3.php

---
+++ index3.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+
+	<head>
+
+		<?php
+			//error_reporting(E_ERROR | E_PARSE);
+			include "/var/www/FYP2023.com/include/system/dbConnection.php";
+			include "/var/www/FYP2023.com/include/system/head3.php";
+		?>
+
+	</head>
+	
+	<body>
+
+		<?php
+			include "/var/www/FYP2023.com/include/system/stats.php";
+		?>
+
+	</body>
+
+	<footer>
+
+		<?php
+			include "/var/www/FYP2023.com/include/system/footer.php";
+		?>
+
+	</footer>
+
+</html>
