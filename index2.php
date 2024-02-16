ddca43f9a7e637f2fc87d9b7b9a0d057b041adda
itisiriri <117071101+itisiriri@users.noreply.github.com>
2024-02-16 22:28:30 +0800

    Create index2.php

---
+++ index2.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+
+	<head>
+
+		<?php
+			//error_reporting(E_ERROR | E_PARSE);
+			include "include/system/dbConnection.php";
+			include "include/class/class.php";
+			include "include/system/head2.php";
+		?>
+
+	</head>
+	
+	<body>
+
+		<?php
+			include "include/system/validate.php";
+		?>
+
+	</body>
+
+	<footer>
+
+		<?php
+			include "include/system/footer.php";
+		?>
+
+	</footer>
+
+</html>
