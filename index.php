afe65ce6436220daa4013ba9625f828657aecfa9
itisiriri <117071101+itisiriri@users.noreply.github.com>
2024-02-16 22:56:19 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,29 +1,31 @@
 <!DOCTYPE html>
 <html>
 
-<head>
-    <title>Debugging index.php</title>
-</head>
-
 <body>
 
     <?php
+    var_dump($_GET);
+
     error_reporting(E_ERROR | E_PARSE);
 
-    //Check if $_GET['data'] is set
     if (isset($_GET['data'])) {
         // Include necessary files
-        echo 'hello';
         include "include/system/dbConnection.php";
         include "include/class/class.php";
         include "include/system/head.php";
 
+        $studentDetail = $student->student_selected($_GET['data']);
+        if (isset($studentDetail['student_id']) && $studentDetail['student_id'] == $_GET['data']) {
+            // Include content.php and timetable listing
             include "include/system/content.php";
+            include "include/module/timetable/listing.php";
+        } else {
+            // Include non-content.php
+            include "include/system/non-content.php";
+        }
 
-        // Include footer.php
         include "include/system/footer.php";
     } else {
-        // Display an error message if $_GET['data'] is not set
         echo "Error: Missing data parameter.";
     }
     ?>
