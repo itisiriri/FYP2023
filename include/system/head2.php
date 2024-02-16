612a31b104dacfcfa189fd6ea3bb8bb1866a5810
itisiriri <117071101+itisiriri@users.noreply.github.com>
2024-02-17 00:02:10 +0800

    Create head2.php

---
+++ include/system/head2.php
@@ -0,0 +1,44 @@
+<!-- default head -->
+
+<title>STUDENT ATTENDANCE LIST</title>
+
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv=content-type content="text/html; charset=utf-8">
+
+<style>
+
+	html, body, h1, h2, h3, h4, h5 {font-family: "Open Sans", sans-serif}
+
+	#footer {
+		position: fixed;
+	    padding: 10px 10px 0px 10px;
+	    bottom: 0;
+	    width: 100%;
+	    /* Height of the footer*/
+	    height: 45px;
+	}
+
+</style>
+
+<!-- default head -->
+
+<!-- plugin -->
+
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://www.w3schools.com/lib/w3-theme-deep-purple.css">
+<link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Open+Sans'>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+
+<script type="text/javascript" src="/fyp/include/plugin/jquery-3.3.1/jquery-3.3.1.min.js"></script>
+<link rel='stylesheet' type='text/css' href='/fyp/include/plugin/DataTables/datatables.min.css' media='screen'/>
+<script type='text/javascript' src='/fyp/include/plugin/DataTables/datatables.min.js'></script>
+<script type='text/javascript' src='/fyp/include/plugin/popper.js-1.14.4/dist/umd/popper.min.js'></script>
+<script type='text/javascript' src='/fyp/include/plugin/bootstrap-4.5.2-dist/js/bootstrap.min.js'></script>
+<script type='text/javascript' src='/fyp/include/plugin/mainjs/mainjs.js'></script>
+<link rel='stylesheet' type='text/css' href='/fyp/include/plugin/DataTables/DataTables-1.10.22/Responsive-2.2.7/css/responsive.dataTables.min.css' media='screen'/>
+<script type='text/javascript' src='/fyp/include/plugin/DataTables/DataTables-1.10.22/Responsive-2.2.7/js/dataTables.responsive.min.js'></script>
+
+<!-- plugin -->
+				
+		
