3b99635d163b1324931468256667ae229cc9d53e
itisiriri <117071101+itisiriri@users.noreply.github.com>
2024-02-16 23:52:15 +0800

    Update index.php

--- index.php
+++ index.php
@@ -5,11 +5,13 @@
 
 		<?php
 
-            $id = "2022937613";
-			error_reporting(E_ERROR | E_PARSE);
-			include "/var/www/FYP2023.com/include/system/dbConnection.php";
-			include "/var/www/FYP2023.com/include/class/class.php";
-			include "/var/www/FYP2023.com/include/system/head.php";
+			// Start measuring time
+    		$start_time = microtime(true);
+
+			//error_reporting(E_ERROR | E_PARSE);
+			include "include/system/dbConnection.php";
+			include "include/class/class.php";
+			include "include/system/head.php";
 		?>
 
 	</head>
@@ -18,14 +20,23 @@
 
 		<?php
 
-			$studentDetail = $student -> student_selected($id);
+		// Code to measure execution time
 
-			if($studentDetail['student_id'] == $id){
-				include "/var/www/FYP2023.com/include/system/content.php";
-				include "/var/www/FYP2023.com/include/module/timetable/listing.php"; 
+			$studentDetail = $student -> student_selected($_GET['data']);
+
+			if($studentDetail['student_id'] == $_GET['data']){
+				include "include/system/content.php";
+				include "include/module/timetable/listing.php"; 
 			} else {
-				include "/var/www/FYP2023.com/include/system/non-content.php";
+				include "include/system/non-content.php";
 			}
+
+		// End measuring time
+	    $end_time = microtime(true);
+	    $execution_time = ($end_time - $start_time) * 1000; // Convert to milliseconds
+
+	    // Output execution time
+	    echo "<p>Execution time: " . number_format($execution_time, 2) . " milliseconds</p>";
 
 		?>
 
@@ -34,7 +45,7 @@
 	<footer>
 
 		<?php
-			include "/var/www/FYP2023.com/include/system/footer.php";
+			include "include/system/footer.php";
 		?>
 
 	</footer>
